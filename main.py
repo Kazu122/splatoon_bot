72badc07071d65dfbb0c9f27231ca121ef76cb04
Kazu122 <75375997+Kazu122@users.noreply.github.com>
2022-12-24 12:19:02 +0900

    コマンドの実装方法確認

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import discord
 import os
 from dotenv import load_dotenv
+from discord import app_commands
+from discord.ext import commands
+from discord_slash import SlashCommand, SlashContext
 
 load_dotenv()
 
 TOKEN = os.environ.get('TOKEN')
 
-intents = discord.Intents.default()  # デフォルトのIntentsオブジェクトを生成
-client = discord.Client(intents=intents)
+intents = discord.Intents.all()  # Intentsオブジェクトを生成
+client = discord.Client(intents=intents, command_prefix='/')
+tree = app_commands.CommandTree(client)
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+    await tree.sync()
 
 @client.event
 async def on_message(message):
@@ -19,5 +24,17 @@ async def on_message(message):
         return
 
     await message.channel.send('Hello!')
+
+@tree.command(name = 'test', description='test')
+async def test(interaction):
+    await interaction.response.send_message(f'hello world')
+    
+@slash.slash(name="test")
+
+async def _test(ctx: SlashContext):
+
+    embed = discord.Embed(title="embed test")
+
+    await ctx.send(content="test", embeds=[embed])
 
 client.run(TOKEN)
